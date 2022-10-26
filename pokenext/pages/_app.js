c164bf10a3fc118081f70edaf75a37ea66eb0c33
ruben.borges.ramos <ruben.borges.ramos@accenture.com>
2022-10-25 22:46:46 -0300

    fix: correcoes de rotas

--- pokenext/pages/_app.js
+++ pokenext/pages/_app.js
@@ -1,7 +1,7 @@
 
 import '../styles/globals.css'
 
-import Layout from '../pages/components/Layout'
+import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
   return (
