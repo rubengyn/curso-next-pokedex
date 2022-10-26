f2f4b90bc28df52f840e398332de961e9567065c
ruben.borges.ramos <ruben.borges.ramos@accenture.com>
2022-10-25 21:53:50 -0300

    feat: Aula 26 - Finalizando páginda de Pokemon

--- pokenext/pages/pokemon/[pokemonId].js
+++ pokenext/pages/pokemon/[pokemonId].js
@@ -1,4 +1,8 @@
-export const getStaticPaths = async() => {
+import Image from 'next/image'
+
+import styles from '../../styles/Pokemon.module.css'
+
+export const getStaticPaths = async () => {
     const maxPokemons = 251
     const api = 'https://pokeapi.co/api/v2/pokemon'
 
@@ -8,7 +12,7 @@ export const getStaticPaths = async() => {
     // params for
     const paths = data.results.map((pokemon, index) => {
         return {
-            params: {pokemonId: (index+1).toString()},
+            params: { pokemonId: (index + 1).toString() },
         }
     })
     return {
@@ -26,10 +30,50 @@ export const getStaticProps = async (context) => {
     const data = await res.json()
 
     return {
-        props: {pokemon: data},
+        props: { pokemon: data },
     }
 }
 
-export default function Pokemon({pokemon}){
-    return <p>{pokemon.name}</p>
+export default function Pokemon({ pokemon }) {
+    return (
+        <div className={styles.pokemon_container}>
+            <h1 className={styles.title}>{pokemon.name}</h1>
+            <Image
+                src={`https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png`}
+                width='200'
+                height='200'
+                alt={pokemon.name}
+            />
+            <div>
+                <h3>Número:</h3>
+                <p>#{pokemon.id}</p>
+            </div>
+            <div>
+                <h3>Tipo:</h3>
+                <div className={styles.types_container}>
+                    {pokemon.types.map((item, index) => (
+                        <span
+                            key={index}
+                            className={
+                                `${styles.type} ${styles['type_' + item.type.name]}`
+                            }
+                        >
+                            {item.type.name}
+                        </span>
+                    ))}
+                </div>
+            </div>
+            <div className={styles.data_container}>
+                <div className={styles.data_height}>
+                    <h4>Altura: </h4>
+                    <p>{pokemon.height * 10} cm</p>
+                </div>
+                <div className={styles.data_weight}>
+                    <h4>Peso: </h4>
+                    <p>{pokemon.weight / 10} kg</p>
+                </div>
+            </div>
+
+        </div>
+    )
 }
